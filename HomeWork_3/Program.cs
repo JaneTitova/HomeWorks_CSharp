ed64a232e68e897ce202196c614efcf39cd7c909
Jane <etitovabit@gmail.com>
2022-05-05 17:57:44 +0800

    All tasks solved

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -27,10 +27,48 @@ else Console.Write(num + " -> не пятизначное число!");
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+/*
+void Distance(double xa, double ya, double za,
+              double xb, double yb, double zb)
+{
+    double d = Math.Sqrt((xb-xa)*(xb-xa) + (yb-ya)*(yb-ya) + (zb-za)*(zb-za));
+    Console.Write("A(" + xa + "," + ya + "," + za + "); B("+ xb + "," + yb + "," + zb + ") -> " + d);
+}
 
+Console.Write("Введите x координату точки А: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y координату точки А: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z координату точки А: ");
+double zA = Convert.ToDouble(Console.ReadLine());
 
+Console.Write("Введите x координату точки B: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y координату точки B: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z координату точки B: ");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+Distance(xA, yA, zA, xB, yB, zB);
+*/
 
 // Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+void Cube(int n)
+{
+    int count = 2;
+    Console.Write(n + " -> 1");
+    while(count < n+1)
+    {
+        Console.Write(", " + count*count*count);
+        count++;
+    }
+}
+
+Console.Write("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 0) Cube(num);
+else Console.Write(num + " -> 0 или отрицательное!");
