33ec5519e303ff31250fcda2437aaab478002a58
Jane <etitovabit@gmail.com>
2022-05-12 02:08:37 +0800

    Third task solved, and some mix of all methods

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -21,7 +21,7 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-int SumOfEvenElements(int[] array)
+int CountOfEvenElements(int[] array)
 {
     int count = 0;
     for(int i=0; i<array.Length; i++)
@@ -33,14 +33,14 @@ int SumOfEvenElements(int[] array)
 
 int[] MyArray = CreateRandomArray(6, 100, 999);
 PrintArray(MyArray);
-Console.Write("Сумма четных элементов массива равна - " + SumOfEvenElements(MyArray));
+Console.Write("Количество четных элементов массива равна - " + CountOfEvenElements(MyArray));
 */
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 //Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
-
+/*
 int[] CreateRandomArray(int size, int min, int max)
 {
     int[] array = new int[size];
@@ -73,8 +73,123 @@ int SumElementsOnUnevenPositions(int[] array)
 int[] MyArray = CreateRandomArray(7, -10, 10);
 PrintArray(MyArray);
 Console.Write("Сумма элементов стоящих, на нечетных позициях равна " + SumElementsOnUnevenPositions(MyArray));
+*/
 
 // Задача 38: Задайте массив вещественных чисел. 
 //Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+/*
+double Difference(double a, double b)
+{
+    return b-a;
+}
 
+double FindMax(double[] array)
+{
+    double max = array[0];
+    for(int i=1; i<array.Length; i++)
+    {
+        if(array[i]>max) max = array[i];
+    }
+    return max;
+}
+
+double FindMin(double[] array)
+{
+    double min = array[0];
+    for(int i=1; i<array.Length; i++)
+    {
+        if(array[i]<min) min = array[i];
+    }
+    return min;
+}
+
+double[] MyArray = new double[5]{3,7,22,2,78};
+
+double minimum = FindMin(MyArray);
+double maximum = FindMax(MyArray);
+Console.Write("Разница между " + maximum + " и " + minimum + " равна " + Difference(minimum, maximum));
+*/
+
+//// все методы вместе
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountOfEvenElements(int[] array)
+{
+    int count = 0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i]%2 == 0) count+=1;
+    }
+    return count;
+}
+
+int SumElementsOnUnevenPositions(int[] array)
+{
+    int sum = 0;
+    for(int i=1; i<array.Length; i+=2)
+    {
+        sum+=array[i];
+    }
+    return sum;
+}
+
+int Difference(int a, int b)
+{
+    return b-a;
+}
+
+int FindMax(int[] array)
+{
+    int max = array[0];
+    for(int i=1; i<array.Length; i++)
+    {
+        if(array[i]>max) max = array[i];
+    }
+    return max;
+}
+
+int FindMin(int[] array)
+{
+    int min = array[0];
+    for(int i=1; i<array.Length; i++)
+    {
+        if(array[i]<min) min = array[i];
+    }
+    return min;
+}
+
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите наименьшее значение элементов массива: ");
+int minval = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите наибольшее значение элементов массива: ");
+int maxval = Convert.ToInt32(Console.ReadLine());
+
+int[] MyArray = CreateRandomArray(size, minval, maxval);
+PrintArray(MyArray);
+
+int minimum = FindMin(MyArray);
+int maximum = FindMax(MyArray);
+Console.WriteLine("Разница между " + maximum + " и " + minimum + " равна " + Difference(minimum, maximum));
+
+Console.WriteLine("Сумма элементов стоящих, на нечетных позициях равна " + SumElementsOnUnevenPositions(MyArray));
+
+Console.WriteLine("Количесвто четных элементов массива равна - " + CountOfEvenElements(MyArray));
